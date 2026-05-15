fa98691fc0d1a743d986a31409d2cec68556f385
BennyPetersPXL <benny.peters@student.pxl.be>
2026-05-15 20:32:42 +0200

    Add files via upload

--- taak_restconf/taak_restconf_deploy.py
+++ taak_restconf/taak_restconf_deploy.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
-Taak RESTCONF - End-to-end automatisering
-Router: LAB-RA04-C02-R01 (172.17.4.65)
+Taak RESTCONF - Deploy hardware
+Router: LAB-RA04-C02-R01 - 172.17.4.65
 """
 
 import sys
@@ -11,53 +11,38 @@ import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-DEVICE_IP  = "172.17.4.65"
-USERNAME   = "admin"
-PASSWORD   = "pxl"
-BASE_URL   = "https://{}/restconf/data/Cisco-IOS-XE-native:native".format(DEVICE_IP)
-HEADERS    = {
+DEVICE_IP = "172.17.4.65"
+USERNAME  = "admin"
+PASSWORD  = "pxl"
+BASE_URL  = "https://{}/restconf/data/Cisco-IOS-XE-native:native".format(DEVICE_IP)
+HEADERS   = {
     "Content-Type": "application/yang-data+json",
     "Accept":       "application/yang-data+json",
 }
 
 GITHUB_URL = "https://raw.githubusercontent.com/BennyPetersPXL/yang/refs/heads/main/taak_restconf/taak_restconf_ios_xe_config.json"
 
-def pretty_print_json(data):
-    """Basisvaardigheid: JSON pretty-print via json.dumps met indent."""
-    try:
-        if isinstance(data, str):
-            data = json.loads(data)
-        return json.dumps(data, indent=2)
-    except Exception:
-        return str(data)
+print("=" * 44)
+print("Taak RESTCONF - {}".format(DEVICE_IP))
+print("=" * 44)
 
 
-def parse_json(data):
-    """Basisvaardigheid: JSON response deserialiseren naar Python dictionary."""
-    result = {}
-    if "Cisco-IOS-XE-native:hostname" in data:
-        result["hostname"] = data["Cisco-IOS-XE-native:hostname"]
-    if "Cisco-IOS-XE-native:GigabitEthernet" in data:
-        result["interfaces"] = [
-            {
-                "naam": i.get("name"),
-                "ip":   i.get("ip", {}).get("address", {})
-                         .get("primary", {}).get("address", "?")
-            }
-            for i in data["Cisco-IOS-XE-native:GigabitEthernet"]
-        ]
-    return result
+# [1] Config ophalen van GitHub
+print("\n[1] Config ophalen van GitHub")
+r = requests.get(GITHUB_URL, timeout=10)
+if r.status_code != 200:
+    print("    FOUT: GitHub niet bereikbaar ({})".format(r.status_code))
+    sys.exit(1)
+print("    {} bytes opgehaald".format(len(r.content)))
 
 
-def controleer_status(naam, response):
-    """Basisvaardigheid: HTTP statuscodes controleren en tonen."""
-    code = response.status_code
-    if code in [200, 201, 204]:
-        print("    HTTP {} - {} geslaagd".format(code, naam))
-    else:
-        print("    HTTP {} - {} mislukt".format(code, naam))
-        print("    Details: {}".format(response.text[:200]))
-        sys.exit(1)
+# [2] Config parsen
+print("\n[2] Config parsen")
+config = r.json()
+print("    hostname:  {}".format(config["hostname"]))
+for intf in config["interfaces"]:
+    print("    interface: {} - {}".format(intf["name"], intf["address"]))
+print("    banner:    {}".format(config.get("banner", "-")))
 
 
 def put(url, body):
@@ -68,87 +53,85 @@ def put(url, body):
     )
 
 
-def verificatie(url, naam):
-    """Basisvaardigheid: GET response ophalen, pretty-print en parsen."""
-    r = requests.get(
-        url, auth=(USERNAME, PASSWORD),
-        headers=HEADERS, verify=False, timeout=15
-    )
-    print("\n    GET {} - HTTP {}".format(naam, r.status_code))
-    if r.status_code == 200 and r.text:
-        data = r.json()
-        print("    Response (pretty-print JSON):")
-        print("    " + "-" * 40)
-        for regel in pretty_print_json(data).splitlines()[:15]:
-            print("    " + regel)
-        print("    " + "-" * 40)
-        parsed = parse_json(data)
-        if parsed:
-            print("    Geparseerde datastructuur (Python dictionary):")
-            for k, v in parsed.items():
-                print("      {}: {}".format(k, v))
+def controleer_status(naam, response):
+    code = response.status_code
+    if code in [200, 201, 204]:
+        print("    HTTP {} - {} geslaagd".format(code, naam))
+    else:
+        print("    HTTP {} - {} mislukt".format(code, naam))
+        print("    Details: {}".format(response.text[:300]))
+        sys.exit(1)
 
 
-def main():
-    print("=" * 45)
-    print("Taak RESTCONF - {}".format(DEVICE_IP))
-    print("=" * 45)
-
-    # Stap 1: Config ophalen van GitHub (single source of truth)
-    print("\n[1] Config ophalen van GitHub")
-    try:
-        r = requests.get(GITHUB_URL, timeout=15)
-        r.raise_for_status()
-        config = r.json()
-    except Exception as e:
-        sys.exit("FOUT: {}".format(e))
-    print("    {} bytes opgehaald".format(len(r.text)))
-
-    # Stap 2: Config parsen (deserialiseren: JSON naar Python dictionary)
-    print("\n[2] Config parsen")
-    print("    hostname:  {}".format(config["hostname"]))
-    for intf in config["interfaces"]:
-        print("    interface: Gi{} - {}".format(intf["name"], intf["address"]))
-
-    # Stap 3: Hostname deployen
-    print("\n[3] Hostname deployen via RESTCONF PUT")
-    r = put("{}/hostname".format(BASE_URL),
-            {"Cisco-IOS-XE-native:hostname": config["hostname"]})
-    controleer_status("hostname", r)
-
-    # Stap 4: Interfaces deployen
-    print("\n[4] Interfaces deployen via RESTCONF PUT")
-    for intf in config["interfaces"]:
-        body = {"Cisco-IOS-XE-native:GigabitEthernet": [{
-            "name":        intf["name"],
-            "description": intf["description"],
-            "ip": {"address": {"primary": {
-                "address": intf["address"],
-                "mask":    intf["mask"]
-            }}}
-        }]}
-        r = put("{}/interface/GigabitEthernet={}".format(BASE_URL, intf["name"]), body)
-        controleer_status("Gi{}".format(intf["name"]), r)
-
-    # Stap 5: OSPF deployen
-    print("\n[5] OSPF deployen via RESTCONF PUT")
-    ospf = config["ospf"]
-    body = {"Cisco-IOS-XE-ospf:ospf": [{
-        "id":        ospf["id"],
-        "router-id": ospf["router_id"],
-        "network":   [{"ip": n["ip"], "mask": n["mask"], "area": n["area"]}
-                      for n in ospf["networks"]]
-    }]}
-    r = put("{}/router/ospf={}".format(BASE_URL, ospf["id"]), body)
-    controleer_status("OSPF", r)
-
-    # Stap 6: Verificatie via RESTCONF GET
-    print("\n[6] Verificatie via RESTCONF GET")
-    verificatie("{}/hostname".format(BASE_URL), "hostname")
-    verificatie("{}/interface".format(BASE_URL), "interfaces")
-
-    print("\nDeployment afgerond.")
+# [3] Hostname deployen
+print("\n[3] Hostname deployen via RESTCONF PUT")
+r = put("{}/hostname".format(BASE_URL),
+        {"Cisco-IOS-XE-native:hostname": config["hostname"]})
+controleer_status("hostname", r)
 
 
-if __name__ == "__main__":
-    main()
+# [4] Banner deployen
+print("\n[4] Banner deployen via RESTCONF PUT")
+body = {
+    "Cisco-IOS-XE-native:banner": {
+        "motd": {"banner": config["banner"]}
+    }
+}
+r = put("{}/banner".format(BASE_URL), body)
+controleer_status("banner", r)
+
+
+# [5] Interfaces deployen
+print("\n[5] Interfaces deployen via RESTCONF PUT")
+for intf in config["interfaces"]:
+    naam = intf["name"]
+    if naam.startswith("Loopback"):
+        url = "{}/interface/Loopback={}".format(BASE_URL, naam.replace("Loopback", ""))
+        body = {
+            "Cisco-IOS-XE-native:Loopback": [{
+                "name":        int(naam.replace("Loopback", "")),
+                "description": intf["description"],
+                "ip": {"address": {"primary": {
+                    "address": intf["address"],
+                    "mask":    intf["mask"]
+                }}}
+            }]
+        }
+    else:
+        url = "{}/interface/GigabitEthernet={}".format(BASE_URL, naam)
+        body = {
+            "Cisco-IOS-XE-native:GigabitEthernet": [{
+                "name":        naam,
+                "description": intf["description"],
+                "ip": {"address": {"primary": {
+                    "address": intf["address"],
+                    "mask":    intf["mask"]
+                }}}
+            }]
+        }
+    r = put(url, body)
+    controleer_status("interface {}".format(naam), r)
+
+
+# [6] OSPF deployen
+print("\n[6] OSPF deployen via RESTCONF PUT")
+ospf = config["ospf"]
+body = {
+    "Cisco-IOS-XE-native:router": {
+        "Cisco-IOS-XE-ospf:ospf": [{
+            "id":        ospf["id"],
+            "router-id": ospf["router_id"],
+            "network":   [
+                {"ip": n["ip"], "mask": n["mask"], "area": n["area"]}
+                for n in ospf["networks"]
+            ]
+        }]
+    }
+}
+r = put("{}/router".format(BASE_URL), body)
+controleer_status("OSPF", r)
+
+
+print("\n" + "=" * 44)
+print("Deployment afgerond!")
+print("=" * 44)
